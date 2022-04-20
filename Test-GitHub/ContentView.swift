4e0fe5f70dfff72cbdb34d83e878bd529441a21e
Marijn Doeve <marijn@doeve.me>
2022-04-20 13:16:09 +0200

    Initial Commit

---
+++ Test-GitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test-GitHub
+//
+//  Created by Marijn Doeve on 20-04-2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test-GitHub/Test_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GitHubApp.swift
+//  Test-GitHub
+//
+//  Created by Marijn Doeve on 20-04-2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
